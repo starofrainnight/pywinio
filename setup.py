c45e94ca45b8c405c77feda968ffde0e487c3558
Hong-She Liang <starofrainnight@gmail.com>
2015-08-18 15:33:32 +0800

    Fixed WinIO word case.

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
     author="Hong-She Liang",
     author_email="starofrainnight@gmail.com",
     url="https://github.com/starofrainnight/%s" % package_name,
-    description="A wrapper library for winio",
+    description="A wrapper library for WinIO",
     long_description=long_description,    
     classifiers=[
         "Development Status :: 3 - Alpha",
