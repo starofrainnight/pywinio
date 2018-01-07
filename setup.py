8059f3aca4aa61b5a1dd7899010a68831e07af08
Hong-She Liang <starofrainnight@gmail.com>
2018-01-07 13:55:55 +0800

    Supported bumpversion (auto tag and commit)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ download_winio_binary()
 
 setup(
     name=package_name,
-    version="0.1.14",
+    version='0.1.14',
     author="Hong-She Liang",
     author_email="starofrainnight@gmail.com",
     url="https://github.com/starofrainnight/%s" % package_name,
