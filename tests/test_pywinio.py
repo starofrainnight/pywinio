e0970e8342cc8967c339a6e5e487b20981063589
Hong-She Liang <starofrainnight@gmail.com>
2018-07-24 11:34:29 +0800

    Fixed wrong importing not exists main module

--- tests/test_pywinio.py
+++ tests/test_pywinio.py
@@ -8,30 +8,7 @@ import pytest
 from click.testing import CliRunner
 
 from pywinio import pywinio  # noqa
-from pywinio.__main__ import main
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(main)
-    assert result.exit_code == 0
-    help_result = runner.invoke(main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+def test_fake():
+    pass
